fbb1f8fa496830b82a75960742eb297812c1eba5
luisben <luisben@email.com>
2014-07-25 08:09:08 -0500

    add readme and codebook
    adding blank readme and codebook md files and the tidy dataset in txt
    format. also did some minor changes to comments in run_analysis.r

--- run_analysis.R
+++ run_analysis.R
@@ -16,7 +16,7 @@ test_measures_file = "./UCI HAR Dataset/test/X_test.txt"
 
 act_labels_file = "./UCI HAR Dataset/activity_labels.txt"
 feats_labels_file = "./UCI HAR Dataset/features.txt"
-##read train and data X files 
+##read train and test X,Y,and subject files 
 train_act_labels = read.table(train_activities_file,row.names=NULL)
 test_act_labels = read.table(test_activities_file,row.names=NULL)
 
@@ -25,18 +25,19 @@ test_subjects = read.table(test_subjects_file,row.names=NULL)
 
 train_measures = read.table(train_measures_file,row.names=NULL)
 test_measures = read.table(test_measures_file,row.names=NULL)
-##read train and data Y files
+##read the activity labels file
 activity_labels <- read.table(act_labels_file)
-##read feature list file
+##read the feature names file
 feature_labels <- read.table(feats_labels_file)
-##subset features labels to those with mean or std in its label
+##subset feature names label vector to those with mean or std in its label
+##the used regexp will get both mean() and meanFreq() features...using "mean\\(" would exclude meanFreq() features
 feature_list <-sapply(as.character(feature_labels$V2), function (str) {  if(grepl("mean",str) || grepl("std",str)) str else NA } ,USE.NAMES=FALSE)
 feature_list <- feature_list[!is.na(feature_list)]
 feature_labels <- as.character(feature_labels$V2)
-##make it the colnames for measures dataframes
+##assign the features labels vector as the colnames for the measures dataframes
 colnames(train_measures) <- feature_labels
 colnames(test_measures) <- feature_labels
-#subset data frame
+##now subset the data frames to only the filtered features
 train_measures <- train_measures[,feature_list]
 test_measures <- test_measures[,feature_list]
 
@@ -49,16 +50,16 @@ train_measures <- cbind(train_act_factor,train_measures)
 ##add subject id to existing tables
 test_measures <- cbind(test_subjects,test_measures)
 train_measures <- cbind(train_subjects,train_measures)
-#rename new columns
+#give names to new columns
 colnames(test_measures)[1] <- "subjectid"
 colnames(train_measures)[1] <- "subjectid"
 colnames(test_measures)[2] <- "activity"
 colnames(train_measures)[2] <- "activity"
-##append both DFs together
+##rowbind both data frames into one
 har_dataset <- rbind(test_measures,train_measures)
 
 ##phase two
 ##create dataset from mean of each var by activity and subject
 harMelt <- melt(har_dataset,id=c("subjectid","activity"))
 HAR_means <- dcast(harMelt,subjectid + activity ~ variable,mean)
-summary(HAR_means)
+write.table(HAR_means,file="tidy_HAR.txt",row.names=FALSE)
